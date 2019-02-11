39b73d5259a270cf147d85802631f254c71d4158
tmuldoon21 <42975519+tmuldoon21@users.noreply.github.com>
2019-02-11 17:49:22 -0500

    Add files via upload

--- Patient.java
+++ Patient.java
@@ -1,4 +1,44 @@
 
-public class Patient {
-
+public class Patient 
+{
+	private String name;
+	private String address; 
+	private double amountOwed;
+	
+	public Patient(String n, String a, double o)
+	{
+		name = n;
+		address = a;
+		amountOwed = o;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getAddress()
+	{
+		return address;
+	}
+	
+	public void setName(String n)
+	{
+		name = n;
+	}
+	public void setAddress(String a)
+	{
+		address = a;
+	}
+	
+	public void newCharge(double o)
+	{
+		amountOwed = amountOwed + o;
+	}
+	
+	public void payment(double o)
+	{
+		amountOwed = amountOwed - o;
+	}
 }
+
